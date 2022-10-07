9d95cb464898c32e542e38ca42745465c5d6f2ca
davidmejia1224 <jose.mejia.mt@usa.edu.co>
2022-10-07 18:54:18 -0500

    avances del front

--- Reto3/src/main/java/com/doctors/controller/UserAdministratorController.java
+++ Reto3/src/main/java/com/doctors/controller/UserAdministratorController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/UserAdministrator")
+@RequestMapping("/api/Admin")
 @CrossOrigin(origins = "*")
 public class UserAdministratorController {
 
