c7b01d15b761b574c9c4e8ed3e06d90111645509
Catmorx <alismora3@gmail.com>
2023-06-07 18:25:55 -0500

    Eliminacion de la carpeta reto 3

--- Reto3/src/main/java/com/doctors/repository/crudrepository/AdminCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.doctors.repository.crudrepository;
-
-import com.doctors.model.AdminModel;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminCrudRepository extends CrudRepository<AdminModel, Integer> {
-}
